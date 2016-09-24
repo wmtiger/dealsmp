c1a18cde2e063ebaf91ef2f5a5b8d936bc7e5d51
wmtiger <bfstiger@gmail.com>
2016-09-24 14:07:07 +0800

    修改扫描像素大小
    让图片更容易被扫描出来

--- zbarreader.py
+++ zbarreader.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- conding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ########################
 #File Name:zbarreader.py
 #Author:WmTiger
@@ -13,26 +13,34 @@ import io
 import zbar
 from PIL import Image
 
-def scanQR(sleeptime=0.5):
+def scanQR(sleeptime=0.5,type = 0):
     stream = io.BytesIO()
     sc = zbar.ImageScanner()
     sc.parse_config("enable")
     with picamera.PiCamera() as c:
-#       c.resolution = (640, 480)
+        c.resolution = (2560, 1440)
         c.start_preview()
         time.sleep(sleeptime)
         c.capture(stream, format='jpeg')
         stream.seek(0)
         pim = Image.open(stream).convert('L')
+ #       pim.save("t1.jpg")
         w, h = pim.size
-        raw = pim.tostring()
-        zim = zbar.Image(w,h,'Y800', raw)
+        if type == 0:
+            box = (w/5,h/10,w/3 * 2, h/10)
+        else:
+            box = (w/4,h/8,w/4 * 3, h/2)
+        cpim = pim.crop(box)
+ #       cpim.save("test1.jpg")
+        cw, ch = cpim.size
+        raw = cpim.tostring()
+        zim = zbar.Image(cw, ch,'Y800', raw)
         sc.scan(zim)
         data = []
         for sb in zim:
             data.append(sb.data)
         del(zim)
-
+ 
         news_ids = []
         dataLen = len(data)
         for i in range(0, dataLen):
@@ -47,25 +55,25 @@ def getHandQR(times=50):
     while times > 0:
         if len(data) >= 2:
             break
-        data = scanQR(0.6)
+        data = scanQR(0.6, 0)
         times -= 1
     print n - times
     return data
 
 def getFlopQR(times=50):
     n = times
-    data = scanQR(0.6)
+    data = scanQR(0.4,1)
     while times > 0:
         if len(data) >= 3:
             break
-        data = scanQR(1)
+    data = scanQR(1)
         times -= 1
     print n - times
     return data
 
 def getTurnQR(times=50):
     n = times
-    data = scanQR(0.6)
+    data = scanQR(0.4,2)
     while times > 0:
         if len(data) >= 4:
             break
@@ -76,7 +84,7 @@ def getTurnQR(times=50):
 
 def getRiverQR(times=50):
     n = times
-    data = scanQR(0.6)
+    data = scanQR(0.4,3)
     while times > 0:
         if len(data) >= 5:
             break
@@ -90,3 +98,4 @@ if __name__ == '__main__':
 #    print getTurnQR()
     print getRiverQR()
 #    print getHandsQR()
+
