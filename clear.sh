74d2859e151b7aa3317adf400c9c28639a30dfde
wmtiger <bfstiger@gmail.com>
2016-09-30 09:30:30 +0800

    aa

---
+++ clear.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+########################
+#File Name:clear.sh
+#Author:WmTiger
+#Mail:bfstiger@gmail.com
+#Created Time:2016-09-30 09:29:02
+########################
+
+rm *.jpg
+rm *.out
+rm *.pyc
